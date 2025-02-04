5562e171bbe8a6567ba7025411e6b29900ef016f
Anubhav Sanyal <princebest3@rediffmail.com>
2025-02-04 17:15:27 +0530

    Added configs file

---
+++ configs.py
@@ -0,0 +1,6 @@
+GITHUB_API_URL='https://api.github.com'
+HARDCODED_REPOSITORY_NAME='cheese-coffee-club-01'
+GITHUB_AUTH_URL="https://github.com/login/oauth/authorize"
+GITHUB_TOKEN_URL="https://github.com/login/oauth/access_token"
+FIRST_COMMIT_MESSAGE="Congratulations on your first dummy commit. You're on the right path"
+FIRST_COMMIT_CONTENT="Isn't great to just fool around"
